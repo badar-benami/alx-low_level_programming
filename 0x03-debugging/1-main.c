635c0bf46147dfa2104f77df7183c450e8f240f9
badar-benami <mindgolden7@gmail.com>
2023-10-10 16:24:53 -0700

    0x03. C - Debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - results in an endless cycle.
+* Return: zero.
+*/
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
