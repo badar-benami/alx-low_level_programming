cb8af19ef5ddd99ea29f0f1f725b648354a9eda8
badar-benami <mindgolden7@gmail.com>
2023-10-12 17:13:49 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_nmbr-Prints an integer.
+ * @n:number to be shown.
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	} else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul-Multiply 2 int.
+ * @a:the first int.
+ * @b:the second int.
+ * Return:the multiplicative addition.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
