0eb27d28a14030ed313ba4d05cd04fc73efe6e45
badar-benami <mindgolden7@gmail.com>
2023-11-03 04:25:33 -0700

    0x0C. C - More malloc, free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -5,7 +5,7 @@
 <<<<<<< HEAD
  * *array_range-----Generates an integer array.
 =======
- * *array_range-*-Generates an integer array.
+ * *array_range-----Generates an integer array.
 >>>>>>> a34a5e00ca98f4d78319b92e8cb8a8d931cc0928
  * @min:Lowest possible range of values kept.
  * @max:Maximum range of values stored and number of elements.
