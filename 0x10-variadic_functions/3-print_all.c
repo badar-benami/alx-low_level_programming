3c80d5eb9ba71abcff0c4745f1e670fad8236df9
badar-benami <mindgolden7@gmail.com>
2023-11-09 15:42:54 -0800

    0x10. C - Variadic functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,88 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * format_char - formats cahracter
- * @separator: the string separator
- * @ap: argument pointer
+ * print_all - Anything may be printed.
+ * @format: A list of the types of parameters supplied to the function.
+ *
+ * Return: No return.
  */
-
-void format_char(char *separator, va_list ap)
-{
-	printf("%s%c", separator, va_arg(ap, int));
-}
-
-/**
- * format_int - formats integer
- * @separator: the string separator
- * @ap: argument pointer
- */
-
-void format_int(char *separator, va_list ap)
-{
-	printf("%s%d", separator, va_arg(ap, int));
-}
-
-/**
- * format_float - formats float
- * @separator: the string separator
- * @ap: argument pointer
- */
-
-void format_float(char *separator, va_list ap)
-{
-	printf("%s%f", separator, va_arg(ap, double));
-}
-
-/**
- * format_string ---string formatting.
- * @separator: the divider between strings.
- * @ap: pointer to an argument.
- */
-
-void format_string(char *separator, va_list ap)
-{
-	char *str = va_arg(ap, char *);
-
-	switch ((int)(!str))
-	case 1:
-		str = "(nil)";
-
-	printf("%s%s", separator, str);
-}
-
-/**
- * print_all - prints anything
- * @format: the format string
- */
-
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
-	char *separator = "";
-	va_list ap;
-	token_t tokens[] = {
-		{"c", format_char},
-		{"i", format_int},
-		{"f", format_float},
-		{"s", format_string},
-		{NULL, NULL}
-	};
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	va_start(ap, format);
+	va_start(valist, format);
 	while (format && format[i])
 	{
 		j = 0;
-		while (tokens[j].token)
+		while (t_arg[j])
 		{
-			if (format[i] == tokens[j].token[0])
+			if (format[i] == t_arg[j] && c)
 			{
-				tokens[j].f(separator, ap);
-				separator = ", ";
-			}
-			j++;
+				printf(", ");
+				break;
+			} j++;
 		}
-		i++;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-	printf("\n");
-	va_end(ap);
+	printf("\n"), va_end(valist);
 }
